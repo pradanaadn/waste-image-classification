9fbaabb9c4b7f301366b912c4d47fa7877fdfa89
pradanaadn <pradanaadn21@gmail.com>
2025-08-04 14:55:56 +0800

    chore: dataset selection

--- roboflow_dataset.py
+++ roboflow_dataset.py
@@ -1,12 +1,25 @@
 from roboflow import Roboflow
+from loguru import logger
 
 LIST_DATASET = [
-    "beverage-containers-3atxb"
+    ("coco", "a-3zezt", "styrofoam-wfuot", 1),
+    ("coco", "ijuden", "kelapa-megatron-kupas", 1),
+    ("coco", "dataixalab", "metal-f7t6c", 1),
+    ("folder", "trash-classification-mjez0", "plastic-hdpe", 1),
+    ("coco", "project-swd48", "plastic-cups-v7jar", 1),
+    ("coco", "cups-iupj5", "cups-ctpw3", 1),
+    ("folder", "recyclable-sorting-algorithm", "plastic-sorting-algorithm", 1),
+    ("folder", "piyush-yadav-gh70a", "pvc-pipe-detection", 1),
+    ("folder", "tiago-rosa", "un-pvc", 1),
 ]
-
-rf = Roboflow(api_key="LRRjhLvhz6FI3dVLj7CK")
-project = rf.workspace("roboflow-universe-projects").project(
-    "beverage-containers-3atxb"
-)
-version = project.version(8)
-dataset = version.download("coco", location="./datasets/roboflow/beverage-containers")
+try:
+    rf = Roboflow(api_key="LRRjhLvhz6FI3dVLj7CK")
+    for output_format, workspace, dataset, version_number in LIST_DATASET:
+        logger.info(f"Downloading dataset: {dataset}")
+        project = rf.workspace(workspace).project(dataset)
+        version = project.version(version_number)
+        dataset = version.download(
+            output_format, location=f"./datasets/roboflow/{dataset}"
+        )
+except Exception as e:
+    logger.error(f"An error occurred while downloading the dataset: {e}")
